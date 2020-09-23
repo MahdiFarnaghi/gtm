4f142c6842600a2da4f8fdfaa06d0611709e1c2f
Mahdi Farnaghi <mahdi.farnaghi@yahoo.com>
2020-09-23 16:50:59 +0200

    The structure for event_detector container was developed and tested.

---
+++ event_detector/test.py
@@ -0,0 +1,11 @@
+for i in range(0, 10):
+    print('='*60)
+
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
+
+for i in range(0, 10):
+    print('='*60)
